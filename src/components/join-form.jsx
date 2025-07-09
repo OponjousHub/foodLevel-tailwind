195bc64a5e53eac2acb3b04cdcd9da7b62399d2a
Oponjousjoe <joe.ikdura@gmail.com>
2025-07-09 07:14:11 +0100

    Addeding the join community button

---
+++ src/components/join-form.jsx
@@ -0,0 +1,31 @@
+function JoinForm() {
+  return (
+    <form className="community-form">
+      <h2>Join community to shear</h2>
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-[12px]">
+          <label htmlFor="name" className="text-amber-50 font-bold text-[1rem]">
+            Your name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className=" border-1 border-solid border-amber-50 p-[7px] text-[#ccc] rounded-[6px]"
+          />
+        </div>
+        <div className="flex flex-col gap-[12px]">
+          <label htmlFor="name" className="text-amber-50 font-bold text-[1rem]">
+            Your Email
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="border-1 border-solid border-amber-50 p-[7px] text-[#ccc] rounded-[6px]"
+          />
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default JoinForm;
--- src/pages/communityPage.jsx
+++ src/pages/communityPage.jsx
@@ -1,19 +1,47 @@
+import JoinForm from "../components/join-form";
+import commuMeal from "../assets/icons/meal.png";
+import commuChef from "../assets/icons/community.png";
+import commuEvent from "../assets/icons/events.png";
+
 function CommunityPage() {
   return (
     <>
-      <div>
-        <h1>
-          One sheared passion: <span>Food</span>
+      <div className="container text-center mt-[5rem]">
+        <h1 className="text-5xl font-bold text-amber-50">
+          One sheared passion: <span className="span">Food</span>
         </h1>
-        <p>Join our community and shear your favorite recipes!</p>
+        <p className="text-amber-100 text-[1.4rem] mt-4">
+          Join our community and shear your favorite recipes!
+        </p>
       </div>
-      <div>
+      <div className="container text-center text-amber-50">
         <h2>Community Perks</h2>
-        <img src="" alt="community recipe" />
-        <p>Shear and discover recipe</p>
-        <img src="" alt="community recipe" />
-        <p>Shear and discover recipe</p>
+        <div className="community-box">
+          <img
+            src={commuMeal}
+            alt="community recipe"
+            className="community-image"
+          />
+          <p>Shear and discover recipe</p>
+        </div>
+        <div className="community-box">
+          <img
+            src={commuChef}
+            alt="community recipe"
+            className="community-image"
+          />
+          <p>Shear and collaborate with cheffs</p>
+        </div>
+        <div className="community-box">
+          <img
+            src={commuEvent}
+            alt="community recipe"
+            className="community-image"
+          />
+          <p>Shear and cook like a cheff</p>
+        </div>
       </div>
+      <JoinForm />
     </>
   );
 }
